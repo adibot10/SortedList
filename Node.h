b22ffdf29f606113dc65501623e33bc411f42956
Adi <adibot10>
2021-06-14 19:23:38 +0300

    Initial commit

---
+++ Node.h
@@ -0,0 +1,19 @@
+//
+// Created by Adi on 13/06/2021.
+//
+
+#ifndef SORTEDLIST_NODE_H
+#define SORTEDLIST_NODE_H
+#include "SortedList.h"
+
+class Node
+{
+    int value;
+    Node *next;
+    friend class SortedList;
+
+};
+
+
+
+#endif //SORTEDLIST_NODE_H
